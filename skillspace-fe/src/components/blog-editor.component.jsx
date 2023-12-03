89d4996355073b5497a390eb44426d6981ee21fb
boyillas <surendra_babu.boyilla@contractors.roche.com>
2023-12-03 18:42:17 +0800

    Editor changes

--- skillspace-fe/src/components/blog-editor.component.jsx
+++ skillspace-fe/src/components/blog-editor.component.jsx
@@ -3,20 +3,63 @@ import logo from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png";
 import { uploadImage } from "../common/aws";
-import { useRef } from "react";
+import { useContext, useEffect } from "react";
+import { Toaster, toast } from "react-hot-toast";
+import { EditorContext } from "../pages/editor.pages";
+import EditorJs from "@editorjs/editorjs";
+import { tools } from "./tools.component";
 
 const BlogEditor = () => {
-  let blogBannerRef = useRef();
+  let {
+    blog,
+    blog: { title, banner, content, tags, des },
+    setBlog,
+  } = useContext(EditorContext);
+
+  useEffect(() => {
+    let editor = new EditorJs({
+      holder: "textEditor",
+      data: "",
+      tools: tools,
+      placeholder: "Let's write an awesome story..",
+    });
+  }, []);
 
   const handleBannerUpload = (e) => {
     let img = e.target.files[0];
     if (img) {
-      uploadImage(img).then((url) => {
-        if (url) {
-          blogBannerRef.current.src = url;
-        }
-      });
+      let loadingToast = toast.loading("Uploading...");
+      uploadImage(img)
+        .then((url) => {
+          if (url) {
+            toast.dismiss(loadingToast);
+            toast.success("Uploaded successfully.");
+            setBlog({ ...blog, banner: url });
+          }
+        })
+        .catch((err) => {
+          toast.dismiss(loadingToast);
+          return toast.error(err);
+        });
     }
+  };
+
+  const handleTitleKeyDown = (e) => {
+    if (e.keyCode == 13) {
+      e.preventDefault();
+    }
+  };
+
+  const handeTitleChange = (e) => {
+    let input = e.target;
+    input.style.height = "auto";
+    input.style.height = input.scrollHeight + "px";
+    setBlog({ ...blog, title: input.value });
+  };
+
+  const handleError = (e) => {
+    let img = e.target;
+    img.src = defaultBanner;
   };
 
   return (
@@ -25,19 +68,22 @@ const BlogEditor = () => {
         <Link to="/" className="flex-none w-10">
           <img src={logo} />
         </Link>
-        <p className="max-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+        <p className="max-md:hidden text-black line-clamp-1 w-full">
+          {title.length ? title : "New Blog"}
+        </p>
         <div className="flex gap-4 ml-auto">
           <button className="btn-dark py-2">Publish</button>
           <button className="btn-light py-2">Save as draft</button>
         </div>
       </nav>
+      <Toaster />
 
       <AnimationWrapper>
         <section>
           <div className="mx-auto max-w-[900px] w-full">
             <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
               <label htmlFor="uploadBanner">
-                <img ref={blogBannerRef} src={defaultBanner} className="z-20" />
+                <img src={banner} className="z-20" onError={handleError} />
                 <input
                   id="uploadBanner"
                   type="file"
@@ -47,6 +93,15 @@ const BlogEditor = () => {
                 />
               </label>
             </div>
+            <textarea
+              placeholder="Blog Title"
+              className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handeTitleChange}
+            ></textarea>
+
+            <hr className="w-full opacity-10 my-5" />
+            <div id="textEditor" className="font-gelasio"></div>
           </div>
         </section>
       </AnimationWrapper>
